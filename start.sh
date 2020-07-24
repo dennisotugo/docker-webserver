360979a2b046284902882ab4a6e7bac4d619456c
Dennis Otugo <otugodennis@gmail.com>
2020-07-24 19:50:58 +0100

    WIP

--- start.sh
+++ start.sh
@@ -70,7 +70,10 @@ then
   cp -Rf /var/config/rabbitmq.conf /etc/supervisord.conf
 elif [[ ! -z "${MPOS}" ]]
 then
-  cp -Rf /var/config/mpos.conf /etc/supervisord.conf
+  cp -Rf /var/config/pos.conf /etc/supervisord.conf
+elif [[ ! -z "${BILLER}" ]]
+then
+  cp -Rf /var/config/biller.conf /etc/supervisord.conf
 else
   cp -Rf /var/config/redis.conf /etc/supervisord.conf
 fi
